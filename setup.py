bd08ed8d3461763697b83a1af0b0cacc92039d9e
David Williams <d.j.williams@dundee.ac.uk>
2020-02-05 14:59:17 +0000

    bug fix

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if cwd.endswith("hamburger") and "hamburger_multithread.py" in files:
     ### now alter the file with the address for the T6SS models
 
     T6SS_core = cwd+"/models/T6SS/T6SS_core.hmm"
-    T6SS_accessory = cwd+"/models/T6SS/T6SS_core.hmm"
+    T6SS_accessory = cwd+"/models/T6SS/T6SS_accessory.hmm"
 
     #now alter the hamburger_multithread.py file:
 
@@ -32,7 +32,12 @@ if cwd.endswith("hamburger") and "hamburger_multithread.py" in files:
     lines.insert(1, 'T6SS_core = "{address}"\n'.format(address = T6SS_core))
     lines.insert(2, 'T6SS_accessory = "{address}"\n'.format(address = T6SS_accessory))
 
+
+    print("Found T6SS models")
+
     ##now write back out:
 
     with open("hamburger_multithread_w_names.py","w") as output:
         output.write(''.join(lines))
+
+    print("T6SS models known to hamburger; inserted into top of script")
